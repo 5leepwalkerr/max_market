80eec09b7f06fe1826ed6db55b6dfdce73adcd75
apachemax <cvb.cvb.2017.cvb@gmail.com>
2023-07-04 17:46:55 +0300

    init commit

---
+++ src/main/java/com/example/max_market/MaxMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.max_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaxMarketApplication.class, args);
+    }
+
+}
