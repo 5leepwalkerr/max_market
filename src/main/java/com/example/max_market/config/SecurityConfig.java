10ed0efa422259243fdf3e78312592f2336ba0eb
apachemax <cvb.cvb.2017.cvb@gmail.com>
2023-07-06 16:05:34 +0300

    change user model & all security files

---
+++ src/main/java/com/example/max_market/config/SecurityConfig.java
@@ -0,0 +1,4 @@
+package com.example.max_market.config;
+
+public class SecurityConfig {
+}
--- src/main/java/com/example/max_market/service/UserService.java
+++ src/main/java/com/example/max_market/service/UserService.java
@@ -2,12 +2,7 @@ package com.example.max_market.service;
 
 import com.example.max_market.model.User;
 
-import java.util.List;
-
 public interface UserService {
-    User findUserById(Long userId);
-    List<User> findAllUsers();
-    boolean saveUser(User user);
-    boolean deleteUser(Long userId);
-    List<User> usergtList(Long idMin);
+
+    boolean createUser(User user);
 }
