3a940f0e5334418a0a569f3d1b867d7eca0f8130
apachemax <cvb.cvb.2017.cvb@gmail.com>
2023-07-09 16:17:14 +0300

    add template & controller for registration

--- src/main/java/com/example/max_market/config/SecurityConfig.java
+++ src/main/java/com/example/max_market/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import com.example.max_market.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -34,10 +33,6 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder(8);
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService)
-                .passwordEncoder(passwordEncoder());
-    }
+
 
 }
--- src/main/java/com/example/max_market/controller/MainController.java
+++ src/main/java/com/example/max_market/controller/MainController.java
@@ -10,4 +10,8 @@ public class MainController {
     public String home(){
         return "home.html";
     }
+    @RequestMapping("/registration")
+    public String registration(){
+        return "registration.html";
+    }
 }
